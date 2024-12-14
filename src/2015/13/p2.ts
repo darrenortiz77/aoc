d7a593c6066b9c4bf8e71a330d5c9b05e0528fd1
Darren Ortiz <darren@darrenortiz.ca>
2024-12-13 21:14:05 -0500

    2015 - day 13

---
+++ src/2015/13/p2.ts
@@ -0,0 +1,94 @@
+/**
+ * https://adventofcode.com/2015/day/13
+ *
+ * General solution:
+ * Same as part 1 except adding a null person and skipping calculations involving that person.
+ * 
+ * DFS to find all possible seating orders.
+ * Calculate happiness of every possible seating order.
+ * Find max.
+ */
+
+import AOCBase from "../../AOCBase";
+
+export default class Solution implements AOCBase {
+  readonly sampleInput = `Alice would gain 54 happiness units by sitting next to Bob.
+Alice would lose 79 happiness units by sitting next to Carol.
+Alice would lose 2 happiness units by sitting next to David.
+Bob would gain 83 happiness units by sitting next to Alice.
+Bob would lose 7 happiness units by sitting next to Carol.
+Bob would lose 63 happiness units by sitting next to David.
+Carol would lose 62 happiness units by sitting next to Alice.
+Carol would gain 60 happiness units by sitting next to Bob.
+Carol would gain 55 happiness units by sitting next to David.
+David would gain 46 happiness units by sitting next to Alice.
+David would lose 7 happiness units by sitting next to Bob.
+David would gain 41 happiness units by sitting next to Carol.`;
+
+  public parseInput(input?: string) {
+    if (!input) {
+      input = this.sampleInput;
+    }
+
+    const happinessMap = new Map<string, Map<string, number>>();
+
+    input.split('\n').map(line => {
+      const matches = line.match(/^(\w+).+(gain|lose) (\d+).+ (\w+)\.$/)!;
+      const person0 = matches[1];
+      const amount = +matches[3] * (matches[2] === 'gain' ? 1 : -1);
+      const person1 = matches[4];
+      const happiness = happinessMap.get(person0) ?? new Map();
+      happiness.set(person1, amount);
+      happinessMap.set(person0, happiness);
+    });
+
+    return happinessMap;
+  }
+
+  public solve(input?: string) {    
+    const performanceStart = performance.now();
+
+    const happinessMap = this.parseInput(input);
+    
+    // Get all the possible combinations of seating order
+    // ======
+    const dfs = (seatingChart: string[], remainingPeople: Set<string>, validSeatings: string[][]) => {
+      if (remainingPeople.size === 0) {
+        validSeatings.push(seatingChart);
+        return;
+      }
+
+      for (const person of remainingPeople) {
+        const remainingClone = new Set(remainingPeople);
+        remainingClone.delete(person);
+        dfs([...seatingChart, person], remainingClone, validSeatings);
+      }
+    };
+
+    const people = new Set(happinessMap.keys());
+    const validSeatings: string[][] = [];
+    dfs([''], people, validSeatings);
+    // ======
+
+    // calculate total happiness for each seating chart
+    const happiness = validSeatings.map(seating => {
+      let sum = 0;
+      for (let i=0; i < seating.length; i++) {
+        const thisPerson = seating[i];
+        const nextPerson = i < seating.length-1 ? seating[i+1] : seating[0];
+        if (thisPerson !== '' && nextPerson !== '') {
+          sum += happinessMap.get(thisPerson)!.get(nextPerson)!;
+          sum += happinessMap.get(nextPerson)!.get(thisPerson)!;
+        }
+      }
+      return sum;
+    });
+
+    const result = Math.max(...happiness);
+
+    return {
+      performance: performance.now() - performanceStart, 
+      result
+    }
+  }
+}
---
+++ src/utils/backtrack.ts
@@ -0,0 +1,61 @@
+const defaultCheckForValid = <T>(current: T, target: T) => {
+	if (current === target) {
+		return true;
+	}
+
+	if (current > target) {
+		return false;
+	}
+
+	return null;
+};
+
+const defaultUpdateCurrent = <T, C>(current: T, candidate: C): T => {
+	return (current as number) + (candidate as number) as T;
+};
+
+/**
+ * A way to recursively check every possible combination of an array of candidates.
+ * 
+ * candidates: list of potentials
+ * initial: initial value
+ * target: some target value we're aiming for
+ * checkForValid: function to test if we've found a valid candidate
+ * updateCurrent: update the current value as we loop through candidates
+ */
+export const backtracker = <C, T>(
+	candidates: C[], 
+	initial: T, 
+	target: T, 
+	checkForValid: (current: T, target: T) => boolean|null = defaultCheckForValid, 
+	updateCurrent: (current: T, candidate: C) => T = defaultUpdateCurrent
+) => {
+	const temp: C[] = []; // temp solutions that gets backtracked over
+	const valids: C[][] = []; // working solutions added here
+
+	const backtrack = (idx: number, current: T) => {
+		const isValid = checkForValid(current, target);
+
+		if (isValid === true) {
+			return valids.push([...temp]);
+		}
+
+		if (isValid === false) {
+			return;
+		}
+
+		// ran out of possibilities, move on
+		if (idx === candidates.length) {
+			return;
+		}
+
+		temp.push(candidates[idx]);
+		backtrack(idx, updateCurrent(current, candidates[idx]));
+		temp.pop();
+		backtrack(idx+1, initial);
+	};
+
+	backtrack(0, initial);
+
+	return valids;
+}
